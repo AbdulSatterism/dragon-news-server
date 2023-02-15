d90e4b43c0cf41b7d658861a6e116154a4cda2bc
Md. Abdul Satter <116937243+AbdulSatterism@users.noreply.github.com>
2023-02-15 23:04:36 +0600

    create api by id

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 
-const categories = require('./data/category.json')
+const categories = require('./data/category.json');
+const news = require('./data/new.json');
 
 app.get('/', (req, res) => {
     res.send('News portal is running on')
@@ -13,6 +14,30 @@ app.get('/', (req, res) => {
 
 app.get('/news-categories', (req, res) => {
     res.send(categories)
+})
+
+// categories wise api create onekgulo category id akoi ase tai filter
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews)
+    }
+
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+//data load by id
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
 })
 
 app.listen(port, () => {
